b9d222a0adf24f7f9c0173d6a77a1806d1ecd1c4
Working123 <krowemoh@hotmail.com>
2015-02-02 20:45:05 -0500

    Adding the Fonts Anywhere bookmarlet to git.

---
+++ js/googleFont.js
@@ -0,0 +1,270 @@
+
+alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+aFonts = ["ABeeZee","Abel","Abril Fatface","Aclonica","Acme","Actor","Adamina","Advent Pro","Aguafina Script","Akronim","Aladin","Aldrich","Alef","Alegreya","Alegreya SC","Alegreya Sans","Alegreya Sans SC","Alex Brush","Alfa Slab One","Alice","Alike","Alike Angular","Allan","Allerta","Allerta Stencil","Allura","Almendra","Almendra Display","Almendra SC","Amarante","Amaranth","Amatic SC","Amethysta","Anaheim","Andada","Andika","Angkor","Annie Use Your Telescope","Anonymous Pro","Antic","Antic Didone","Antic Slab","Anton","Arapey","Arbutus","Arbutus Slab","Architects Daughter","Archivo Black","Archivo Narrow","Arimo","Arizonia","Armata","Artifika","Arvo","Asap","Asset","Astloch","Asul","Atomic Age","Aubrey","Audiowide","Autour One","Average","Average Sans","Averia Gruesa Libre","Averia Libre","Averia Sans Libre","Averia Serif Libre"];
+bFonts = ["Bad Script","Balthazar","Bangers","Basic","Battambang","Baumans","Bayon","Belgrano","Belleza","BenchNine","Bentham","Berkshire Swash","Bevan","Bigelow Rules","Bigshot One","Bilbo","Bilbo Swash Caps","Bitter","Black Ops One","Bokor","Bonbon","Boogaloo","Bowlby One","Bowlby One SC","Brawler","Bree Serif","Bubblegum Sans","Bubbler One","Buda","Buenard","Butcherman","Butterfly Kids"];
+cFonts = ["Cabin","Cabin Condensed","Cabin Sketch","Caesar Dressing","Cagliostro","Calligraffitti","Cambo","Candal","Cantarell","Cantata One","Cantora One","Capriola","Cardo","Carme","Carrois Gothic","Carrois Gothic SC","Carter One","Caudex","Cedarville Cursive","Ceviche One","Changa One","Chango","Chau Philomene One","Chela One","Chelsea Market","Chenla","Cherry Cream Soda","Cherry Swash","Chewy","Chicle","Chivo","Cinzel","Cinzel Decorative","Clicker Script","Coda","Coda Caption","Codystar","Combo","Comfortaa","Coming Soon","Concert One","Condiment","Content","Contrail One","Convergence","Cookie","Copse","Corben","Courgette","Cousine","Coustard","Covered By Your Grace","Crafty Girls","Creepster","Crete Round","Crimson Text","Croissant One","Crushed","Cuprum","Cutive","Cutive Mono"];
+dFonts = ["Damion","Dancing Script","Dangrek","Dawning of a New Day","Days One","Delius","Delius Swash Caps","Delius Unicase","Della Respira","Denk One","Devonshire","Dhurjati","Didact Gothic","Diplomata","Diplomata SC","Domine","Donegal One","Doppio One","Dorsa","Dosis","Dr Sugiyama","Droid Sans","Droid Sans Mono","Droid Serif","Duru Sans","Dynalight"];
+eFonts = ["EB Garamond","Eagle Lake","Eater","Economica","Ek Mukta","Electrolize","Elsie","Elsie Swash Caps","Emblema One","Emilys Candy","Engagement","Englebert","Enriqueta","Erica One","Esteban","Euphoria Script","Ewert","Exo","Exo 2","Expletus Sans"];
+fFonts = ["Fanwood Text","Fascinate","Fascinate Inline","Faster One","Fasthand","Fauna One","Federant","Federo","Felipa","Fenix","Finger Paint","Fira Mono","Fira Sans","Fjalla One","Fjord One","Flamenco","Flavors","Fondamento","Fontdiner Swanky","Forum","Francois One","Freckle Face","Fredericka the Great","Fredoka One","Freehand","Fresca","Frijole","Fruktur","Fugaz One"];
+gFonts = ["GFS Didot","GFS Neohellenic","Gabriela","Gafata","Galdeano","Galindo","Gentium Basic","Gentium Book Basic","Geo","Geostar","Geostar Fill","Germania One","Gidugu","Gilda Display","Give You Glory","Glass Antiqua","Glegoo","Gloria Hallelujah","Goblin One","Gochi Hand","Gorditas","Goudy Bookletter 1911","Graduate","Grand Hotel","Gravitas One","Great Vibes","Griffy","Gruppo","Gudea","Gurajada"];
+hFonts = ["Habibi","Halant","Hammersmith One","Hanalei","Hanalei Fill","Handlee","Hanuman","Happy Monkey","Headland One","Henny Penny","Herr Von Muellerhoff","Hind","Holtwood One SC","Homemade Apple","Homenaje"];
+iFonts = ["IM Fell DW Pica","IM Fell DW Pica SC","IM Fell Double Pica","IM Fell Double Pica SC","IM Fell English","IM Fell English SC","IM Fell French Canon","IM Fell French Canon SC","IM Fell Great Primer","IM Fell Great Primer SC","Iceberg","Iceland","Imprima","Inconsolata","Inder","Indie Flower","Inika","Irish Grover","Istok Web","Italiana","Italianno"];
+jFonts = ["Jacques Francois","Jacques Francois Shadow","Jim Nightshade","Jockey One","Jolly Lodger","Josefin Sans","Josefin Slab","Joti One","Judson","Julee","Julius Sans One","Junge","Jura","Just Another Hand","Just Me Again Down Here"];
+kFonts = ["Kalam","Kameron","Kantumruy","Karla","Karma","Kaushan Script","Kavoon","Kdam Thmor","Keania One","Kelly Slab","Kenia","Khand","Khmer","Kite One","Knewave","Kotta One","Koulen","Kranky","Kreon","Kristi","Krona One"];
+lFonts = ["La Belle Aurore","Laila","Lakki Reddy","Lancelot","Lato","League Script","Leckerli One","Ledger","Lekton","Lemon","Libre Baskerville","Life Savers","Lilita One","Lily Script One","Limelight","Linden Hill","Lobster","Lobster Two","Londrina Outline","Londrina Shadow","Londrina Sketch","Londrina Solid","Lora","Love Ya Like A Sister","Loved by the King","Lovers Quarrel","Luckiest Guy","Lusitana","Lustria"];
+mFonts = ["Macondo","Macondo Swash Caps","Magra","Maiden Orange","Mako","Mallanna","Mandali","Marcellus","Marcellus SC","Marck Script","Margarine","Marko One","Marmelad","Marvel","Mate","Mate SC","Maven Pro","McLaren","Meddon","MedievalSharp","Medula One","Megrim","Meie Script","Merienda","Merienda One","Merriweather","Merriweather Sans","Metal","Metal Mania","Metamorphous","Metrophobic","Michroma","Milonga","Miltonian","Miltonian Tattoo","Miniver","Miss Fajardose","Modern Antiqua","Molengo","Molle","Monda","Monofett","Monoton","Monsieur La Doulaise","Montaga","Montez","Montserrat","Montserrat Alternates","Montserrat Subrayada","Moul","Moulpali","Mountains of Christmas","Mouse Memoirs","Mr Bedfort","Mr Dafoe","Mr De Haviland","Mrs Saint Delafield","Mrs Sheppards","Muli","Mystery Quest"];
+nFonts = ["NTR","Neucha","Neuton","New Rocker","News Cycle","Niconne","Nixie One","Nobile","Nokora","Norican","Nosifer","Nothing You Could Do","Noticia Text","Noto Sans","Noto Serif","Nova Cut","Nova Flat","Nova Mono","Nova Oval","Nova Round","Nova Script","Nova Slim","Nova Square","Numans","Nunito"];
+oFonts = ["Odor Mean Chey","Offside","Old Standard TT","Oldenburg","Oleo Script","Oleo Script Swash Caps","Open Sans","Open Sans Condensed","Oranienbaum","Orbitron","Oregano","Orienta","Original Surfer","Oswald","Over the Rainbow","Overlock","Overlock SC","Ovo","Oxygen","Oxygen Mono"];
+pFonts = ["PT Mono","PT Sans","PT Sans Caption","PT Sans Narrow","PT Serif","PT Serif Caption","Pacifico","Paprika","Parisienne","Passero One","Passion One","Pathway Gothic One","Patrick Hand","Patrick Hand SC","Patua One","Paytone One","Peddana","Peralta","Permanent Marker","Petit Formal Script","Petrona","Philosopher","Piedra","Pinyon Script","Pirata One","Plaster","Play","Playball","Playfair Display","Playfair Display SC","Podkova","Poiret One","Poller One","Poly","Pompiere","Pontano Sans","Port Lligat Sans","Port Lligat Slab","Prata","Preahvihear","Press Start 2P","Princess Sofia","Prociono","Prosto One","Puritan","Purple Purse"];
+qFonts = ["Quando","Quantico","Quattrocento","Quattrocento Sans","Questrial","Quicksand","Quintessential","Qwigley"];
+rFonts = ["Racing Sans One","Radley","Rajdhani","Raleway","Raleway Dots","Ramabhadra","Ramaraja","Rambla","Rammetto One","Ranchers","Rancho","Rationale","Ravi Prakash","Redressed","Reenie Beanie","Revalia","Ribeye","Ribeye Marrow","Righteous","Risque","Roboto","Roboto Condensed","Roboto Slab","Rochester","Rock Salt","Rokkitt","Romanesco","Ropa Sans","Rosario","Rosarivo","Rouge Script","Rozha One","Rubik Mono One","Rubik One","Ruda","Rufina","Ruge Boogie","Ruluko","Rum Raisin","Ruslan Display","Russo One","Ruthie","Rye"];
+sFonts = ["Sacramento","Sail","Salsa","Sanchez","Sancreek","Sansita One","Sarina","Sarpanch","Satisfy","Scada","Schoolbell","Seaweed Script","Sevillana","Seymour One","Shadows Into Light","Shadows Into Light Two","Shanti","Share","Share Tech","Share Tech Mono","Shojumaru","Short Stack","Siemreap","Sigmar One","Signika","Signika Negative","Simonetta","Sintony","Sirin Stencil","Six Caps","Skranji","Slabo 13px","Slabo 27px","Slackey","Smokum","Smythe","Sniglet","Snippet","Snowburst One","Sofadi One","Sofia","Sonsie One","Sorts Mill Goudy","Source Code Pro","Source Sans Pro","Source Serif Pro","Special Elite","Spicy Rice","Spinnaker","Spirax","Squada One","Sree Krushnadevaraya","Stalemate","Stalinist One","Stardos Stencil","Stint Ultra Condensed","Stint Ultra Expanded","Stoke","Strait","Sue Ellen Francisco","Sunshiney","Supermercado One","Suranna","Suravaram","Suwannaphum","Swanky and Moo Moo","Syncopate"];
+tFonts = ["Tangerine","Taprom","Tauri","Teko","Telex","Tenali Ramakrishna","Tenor Sans","Text Me One","The Girl Next Door","Tienne","Timmana","Tinos","Titan One","Titillium Web","Trade Winds","Trocchi","Trochut","Trykker","Tulpen One"];
+uFonts = ["Ubuntu","Ubuntu Condensed","Ubuntu Mono","Ultra","Uncial Antiqua","Underdog","Unica One","UnifrakturCook","UnifrakturMaguntia","Unkempt","Unlock","Unna"];
+vFonts = ["VT323","Vampiro One","Varela","Varela Round","Vast Shadow","Vesper Libre","Vibur","Vidaloka","Viga","Voces","Volkhov","Vollkorn","Voltaire"];
+wFonts = ["Waiting for the Sunrise","Wallpoet","Walter Turncoat","Warnes","Wellfleet","Wendy One","Wire One"];
+xFonts = [];
+yFonts = ["Yanone Kaffeesatz","Yellowtail","Yeseva One","Yesteryear"];
+zFonts = ["Zeyada"];
+
+var chosen="";
+var blue="blue";
+var white="white";
+if (document.getElementById("fontsAnywhere7921904"))
+{
+  document.getElementById("fontsAnywhere7921904").remove();
+}
+else
+{
+  createFontsAnywhere();
+}
+/* Create the input box*/
+function createFontsAnywhere()
+{
+  var textBox = document.createElement("div");
+  textBox.id='fontsAnywhere7921904';
+
+  textBox.innerHTML="<div id='fontsAnywhere'><input id='googleLink' onclick='this.select()'><input id='fontFace' onclick='this.select()'><button id='closeFont' onclick='closeFont()'>x</button><br><input id='fontInput' type='text' onkeypress='getKey(event,1)' onkeyup='getKey(event,0)'><button id='randomFont' onclick='randomFont()'>*</button><div><div id='alphabet'></div><div id='dropDown'></di></div></div>";
+  textBox.style.position="fixed";
+  textBox.style.fontSize="16px";
+  textBox.style.fontFamily="serif";
+  textBox.style.bottom="0%";
+  textBox.style.left="10px";
+  document.body.appendChild(textBox);
+
+  var fontsAnywhere = document.getElementById("fontsAnywhere");
+  fontsAnywhere.style.backgroundColor=white;
+  fontsAnywhere.style.padding="5px";
+  fontsAnywhere.style.border="5px solid #D6D6D6";
+  fontsAnywhere.style.borderRadius="10px";
+
+  var closeFont = document.getElementById("closeFont");
+  closeFont.style.display="inline-block";
+
+  var googleLink = document.getElementById("googleLink");
+  googleLink.style.display="inline-block";
+  googleLink.style.fontFamily="serif";
+  googleLink.style.width="120px";
+  googleLink.style.fontSize="12px";
+  googleLink.style.marginRight="3px";
+
+  googleLink.style.marginBottom="5px";
+  googleLink.style.whiteSpace="nowrap";
+  googleLink.style.overflowX="scroll";
+  googleLink.style.overflowY="hidden";
+
+  var fontFace = document.getElementById("fontFace");
+  fontFace.style.display="inline-block";
+  fontFace.style.fontFamily="serif";
+  fontFace.style.width="57px";
+  fontFace.style.fontSize="12px";
+  fontFace.style.marginRight="5px";
+  fontFace.style.whiteSpace="nowrap";
+  fontFace.style.overflowX="scroll";
+  fontFace.style.overflowY="hidden";
+
+  var closeFont = document.getElementById("closeFont");
+  closeFont.style.fontFamily="serif";
+  closeFont.style.fontSize="16px";
+
+  var randomFont = document.getElementById("randomFont");
+  randomFont.style.fontFamily="serif";
+  randomFont.style.fontSize="16px";
+
+
+
+  var fontInput = document.getElementById("fontInput");
+  fontInput.style.textAlign="center";
+  fontInput.style.fontFamily="serif";
+  fontInput.style.width="180px";
+  fontInput.style.marginRight="5px";
+  fontInput.style.marginBottom="5px";
+  fontInput.value ="a";
+
+  var alphabet = document.getElementById("alphabet");
+  alphabet.style.display = "inline-block";
+  alphabet.style.borderRight = "1px solid black";
+
+  for (i in alpha)
+  {
+    var letter=alpha[i];
+    alphabet.innerHTML += "<div id='"+letter+"'style='margin:0;cursor:pointer;line-height:1;' onmouseout='changeColor(\""+letter+"\",0)' onmouseover='changeColor(\""+letter+"\",1)' onclick='setInput(\""+letter+"\")'>"+letter+"</div>";
+  }
+
+ var dropDown = document.getElementById("dropDown");
+ dropDown.style.width="200px";
+ dropDown.style.overflowX="hidden";
+ dropDown.style.overflowY="scroll";
+
+
+  searchDict();
+}
+
+/* Get key events, specifically the enter key */
+function getKey(e,keypress)
+{
+  if (keypress) //if the key is pressed and its one of these
+  {
+    var input = document.getElementById("fontInput").value;
+    if (e.keyCode ==13) //enter
+    {
+      changeFont(input);
+    }
+    else if (e.keyCode ==40) //down
+    {
+      getNext(input,1);
+    }
+    else if(e.keyCode ==38) //up
+    {
+      getNext(input,-1);
+    }
+  }
+  else
+  {
+    if(e.keyCode!=40 && e.keyCode!=38) //if the key is up and its not the arrows
+    {
+      searchDict();
+    }
+  }
+}
+function closeFont()
+{
+  var child = document.getElementById("fontsAnywhere7921904");
+  child.remove();
+}
+function randomRange(min,max)
+{
+  return Math.floor(Math.random() * (max-min+1)+min);
+}
+function randomFont()
+{
+  var rLetter = String.fromCharCode(randomRange(97,122))+"Fonts";
+  var font = randomRange(0,Object.keys(window[rLetter]).length)
+  var input = window[rLetter][font];
+  changeFont(input);
+  searchDict();
+}
+/* Gets the next font in the list when using the arrow keys */
+function getNext(input,inc)
+{
+  var fontSet = window[input[0].toLowerCase()+"Fonts"];
+  var pos = fontSet.indexOf(input);
+  if (pos>-1)
+  {
+    if (pos+inc>=Object.keys(fontSet).length)
+    {
+      fontSet = window[String.fromCharCode(input[0].charCodeAt(0)+1).toLowerCase()+"Fonts"];
+      pos=-1;
+    }
+    if (pos+inc<0)
+    {
+      fontSet = window[String.fromCharCode(input[0].charCodeAt(0)-1).toLowerCase()+"Fonts"];
+      pos=Object.keys(fontSet).length;
+    }
+
+    var fontInput = document.getElementById("fontInput");
+    fontInput.value = fontSet[pos+inc];
+    changeFont(fontSet[pos+inc]);
+    searchDict(fontInput.value[0]);
+    document.getElementById(fontInput.value).style.backgroundColor=blue;
+
+  }
+}
+/*Sets the input to the letter when the alphabet is used */
+function setInput(letter)
+{
+  document.getElementById("fontInput").value=letter;
+  searchDict();
+}
+
+/* Searches the dictionary every time a key is release */
+function searchDict(input="")
+{
+  var value="";
+  if (input=="")
+  {
+    input = document.getElementById("fontInput").value;
+  }
+  var dict = window[input[0].toLowerCase()+"Fonts"];
+
+  for (i=0;i<Object.keys(dict).length;i++)
+  {
+    if (dict[i].toUpperCase().indexOf(input.toUpperCase()) > -1)
+    {
+      fontName= dict[i]
+      value += "<ul style='margin:0;cursor:pointer;padding:0 0 0 5px;line-height:1;' id='"+fontName+"' onmouseout='changeColor(\""+fontName+"\",0)' onmouseover='changeColor(\""+fontName+"\",1)' onclick='changeFont(\""+fontName+"\")'>"+fontName+"</ul>";
+    }
+  }
+  var dropDown = document.getElementById("dropDown");
+  dropDown.innerHTML = value;
+  dropDown.style.display="inline-block";
+  dropDown.style.float="right";
+  dropDown.style.height=document.getElementById("alphabet").clientHeight+"px";
+  }
+
+
+/* Changes the color of background when a font is hovered over */
+function changeColor(fontName,over)
+{
+  if (fontName!=chosen)
+  {
+    if (over)
+    {
+      document.getElementById(fontName).style.backgroundColor=blue;
+    }
+    else
+    {
+      document.getElementById(fontName).style.backgroundColor=white;
+    }
+  }
+}
+
+/* Gets the google link and changes the font to the new font, only on the body selector */ 
+function changeFont(input)
+{
+  var link = document.createElement("link");
+  link.type="text/css";
+  link.rel="stylesheet";
+  link.href = "http://fonts.googleapis.com/css?family="+input;
+
+  document.getElementsByTagName("head")[0].appendChild(link);
+  document.body.style.fontFamily=input;
+
+  var googleLink = "<link href='"+link.href+"' rel='stylesheet' type='text/css'>";
+  document.getElementById("googleLink").value = googleLink;
+  document.getElementById("fontFace").value = "font-family:'"+input+"';";
+  document.getElementById("fontInput").value = input;
+
+  if (chosen !="")
+  {
+    if (document.getElementById(chosen))
+    {
+      document.getElementById(chosen).style.backgroundColor=white;
+    }
+  }
+  chosen=input;
+
+}
+
+
