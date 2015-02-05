c20e165ca70e94b25fba73aa0130749fac9d52d2
Working123 <krowemoh@hotmail.com>
2015-02-05 13:56:39 -0500

    Added a load script function so that the script shows automatically when a width is larger than 1300. Added hover effects for the close and random.

--- js/loadScript.js
+++ js/loadScript.js
@@ -3,7 +3,7 @@ function checkWidth()
   if (window.innerWidth>1300)
   {
     script = document.createElement("script");
-    script.src="http://127.0.0.1/projects/fonts-anywhere/js/fontsAnywhere.js";
+    script.src="http://www.projectfragments.com/projects/fontsAnywhere/js/fontsAnywhere.js";
     document.body.appendChild(script);
   }
 }
